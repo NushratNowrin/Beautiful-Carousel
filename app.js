8cc479bd6fbea63f2f9da9237f6d2d2bc613a240
NushratNowrin <nowrin2023@gmail.com>
2024-09-26 23:43:37 +0600

    DOM done

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 let nextButton = document.getElementById("next");
 let prevButton = document.getElementById("prev");
+let backButton = document.getElementById("back");
+let seeMoreButtons = document.querySelectorAll(".see-more-btn");
+let carousel = document.querySelector(".carousel");
+let listHTML = document.querySelector(".carousel .list");
