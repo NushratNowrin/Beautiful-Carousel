d6938de0849f9b9fd158ade93179d92f18f345c0
NushratNowrin <nowrin2023@gmail.com>
2024-09-26 23:50:38 +0600

    showSlider done

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ let nextButton = document.getElementById("next");
 let prevButton = document.getElementById("prev");
 let backButton = document.getElementById("back");
 let seeMoreButtons = document.querySelectorAll(".see-more-btn");
-let carousel = document.querySelector(".carousel");
-let listHTML = document.querySelector(".carousel .list");
+let carousel = document.querySelector(".carousel-container");
+let listHTML = document.querySelector(".carousel-container .list");
 
 nextButton.onclick = function () {
 	showSlider("next");
 };
 prevButton.onclick = function () {
 	showSlider("prev");
+};
+const showSlider = (type) => {
+	let items = document.querySelectorAll(".carousel-container, .list, .item");
 };
