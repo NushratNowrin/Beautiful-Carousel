3046cdfb0f35d817f4cedb077ba392b3ae58886e
NushratNowrin <nowrin2023@gmail.com>
2024-09-29 23:49:25 +0600

    bug fixed

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ prevButton.onclick = function () {
 	showSlider("prev");
 };
 const showSlider = (type) => {
-	let items = document.querySelectorAll(".carousel-container, .list, .item");
+	let items = document.querySelectorAll(".carousel-container .list .item");
 	if (type === "next") {
 		listHTML.appendChild(items[0]);
 	}
